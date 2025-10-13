0d2e021cd5b044388bac9770ff6b682746b35120
Ubuntu <ubuntu@ip-172-31-24-147.us-east-2.compute.internal>
2025-10-13 20:10:11 +0000

    ec2 shell script

---
+++ script_ss.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+<< comment
+Annything written here will not execute
+comment
+
+
+name = "Manshi"
+
+echo "Name is $name, and date is $(date)"
+
+echo "enter the name:"
+
+read username
+
+echo "Ypu entered $username"
+
+echo "The characters in $0 are: $1 $2"
